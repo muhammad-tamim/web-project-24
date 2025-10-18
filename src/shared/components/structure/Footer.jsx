24dea07b1bb4f0666b5f71f7c5706c371068f841
muhammad-tamim <contact2tamim@gmail.com>
2025-10-18 13:59:45 +0600

    feat(src/shared/components/structure/): add footer

--- src/shared/components/structure/Footer.jsx
+++ src/shared/components/structure/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className='footer footer-center p-4 bg-base-200 shadow-lg text-base-content'>
+            <aside>
+                <p>Copyright © 2024 - All right reserved by ByteBlaze</p>
+            </aside>
+        </footer>
     );
 };
 
