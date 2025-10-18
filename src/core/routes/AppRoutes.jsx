7709b385ff209418f58af753fb2d4741566d3948
muhammad-tamim <contact2tamim@gmail.com>
2025-10-18 14:01:49 +0600

    feat(src/shared/components/structure/): add hero

--- src/core/routes/AppRoutes.jsx
+++ src/core/routes/AppRoutes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router";
 import MainLayout from "../layouts/MainLayout";
 import HomePage from "../../pages/HomePage";
+import BlogPage from "../../pages/BlogPage";
+import BookmarksPage from "../../pages/BookmarksPage";
+import BlogDetailsPage from "../../pages/BlogDetailsPage";
 
 export const AppRoutes = createBrowserRouter([
     {
@@ -10,7 +13,19 @@ export const AppRoutes = createBrowserRouter([
             {
                 path: '/',
                 Component: HomePage
-            }
+            },
+            {
+                path: 'blogs',
+                Component: BlogPage
+            },
+            {
+                path: 'blog-details',
+                Component: BlogDetailsPage
+            },
+            {
+                path: 'bookmarks',
+                Component: BookmarksPage
+            },
         ]
     },
 ])
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../shared/components/structure/Hero';
+import wave from '../assets/images/wave.svg'
 
 const HomePage = () => {
     return (
-        <div>
+        <div className='flex relative flex-col items-center justify-center min-h-[calc(100vh-52px)]'>
             <Hero></Hero>
+            <img className='absolute bottom-0 w-full' src={wave} alt="" />
         </div>
     );
 };
